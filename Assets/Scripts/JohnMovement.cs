804c6bd9a25772b74781311a580e15d6734b9293
unknown <estilin>
2023-10-21 12:28:55 -0500

    Actualizacion 1228

--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class JohnMovement : MonoBehaviour
 {   //Public significa que se puede ver en el programa directamen
+    public GameObject BulletPrefab;
     public float Speed;
     public float JumpForce;
 
@@ -12,6 +13,8 @@ public class JohnMovement : MonoBehaviour
     private float Horizontal;
     //crear variable global tipo bool true o false
     private bool Grounded;
+    //crear solo una bala
+    private float LastShoot;
 
     private void Start()
     {
@@ -42,13 +45,27 @@ public class JohnMovement : MonoBehaviour
         {
             Jump();
         }
+
+        if(Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
+
     }
 
         private void Jump()
         {
             Rigidbody2D.AddForce(Vector2.up * JumpForce);
         }
-
+        private void Shoot()
+        {   
+            Vector3 direction;
+            if(transform.localScale.x == 1.0f) direction = Vector2.right;
+            else direction = Vector2.left;
+            GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+            bullet.GetComponent<BulletScript>().SetDirection(direction);
+        }
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
