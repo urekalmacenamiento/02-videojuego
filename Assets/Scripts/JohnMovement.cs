5847d2719103298a8f4701cd439412103c4d74e5
unknown <estilin>
2023-10-21 12:27:13 -0500

    Actualizacion 21 octubre

--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -8,11 +8,15 @@ public class JohnMovement : MonoBehaviour
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
+    //crear variable global tipo bool true o false
+    private bool Grounded;
 
     private void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,8 +24,21 @@ public class JohnMovement : MonoBehaviour
         // Movimiento
         Horizontal = Input.GetAxisRaw("Horizontal");
 
+        if(Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        Animator.SetBool("running", Horizontal != 0.0f);
+        //Es para ver el resultado raycast
+        Debug.DrawRay(transform.position,Vector3.down * 0.1f, Color.red);
+
+        //raycast lanza rayo hacia abajo, devuelve true o false para bri 1
+        if(Physics2D.Raycast(transform.position,Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
         //Salto
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             Jump();
         }
